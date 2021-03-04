36685d31f8899fca68da639907b22d853b715859
Serena Cheng <serena.ch1999@gmail.com>
2021-03-04 16:44:45 -0500

    Update activities

--- app/src/main/java/com/example/minigolfscoringapp/ScoreSheetActivity.java
+++ app/src/main/java/com/example/minigolfscoringapp/ScoreSheetActivity.java
@@ -3,12 +3,29 @@ package com.example.minigolfscoringapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ScoreSheetActivity extends AppCompatActivity {
+    EditText p1Hole1, p1Hole2, p1Hole3, p1Hole4, p1Hole5;
+    EditText p2Hole1, p2Hole2, p2Hole3, p2Hole4, p2Hole5;
+    TextView p1Total, p2Total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_sheet);
+        p1Hole1 = (EditText) findViewById(R.id.p1h1);
+        p1Hole2 = (EditText) findViewById(R.id.p1h2);
+        p1Hole3 = (EditText) findViewById(R.id.p1h3);
+        p1Hole4 = (EditText) findViewById(R.id.p1h4);
+        p1Hole5 = (EditText) findViewById(R.id.p1h5);
+
+        p2Hole1 = (EditText) findViewById(R.id.p2h1);
+        p2Hole2 = (EditText) findViewById(R.id.p2h2);
+        p2Hole3 = (EditText) findViewById(R.id.p2h3);
+        p2Hole4 = (EditText) findViewById(R.id.p2h4);
+        p2Hole5 = (EditText) findViewById(R.id.p2h5);
+
     }
 }
--- app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
+++ app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
@@ -8,28 +8,12 @@ import android.widget.Button;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    EditText p1Hole1, p1Hole2, p1Hole3, p1Hole4, p1Hole5;
-    EditText p2Hole1, p2Hole2, p2Hole3, p2Hole4, p2Hole5;
-    TextView p1Total, p2Total;
+public class WinnerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        p1Hole1 = (EditText) findViewById(R.id.editTextNumber2);
-        p1Hole2 = (EditText) findViewById(R.id.editTextNumber5);
-        p1Hole3 = (EditText) findViewById(R.id.editTextNumber6);
-        p1Hole4 = (EditText) findViewById(R.id.editTextNumber8);
-        p1Hole5 = (EditText) findViewById(R.id.editTextNumber11);
-
-        p2Hole1 = (EditText) findViewById(R.id.editTextNumber3);
-        p2Hole2 = (EditText) findViewById(R.id.editTextNumber4);
-        p2Hole3 = (EditText) findViewById(R.id.editTextNumber7);
-        p2Hole4 = (EditText) findViewById(R.id.editTextNumber9);
-        p2Hole5 = (EditText) findViewById(R.id.editTextNumber10);
-
-
+        setContentView(R.layout.activity_winner);
     }
 }
 
