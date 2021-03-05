cb388d51902067c73633580568a440b2aafa49c1
Claire Paisley <paisleyce2@vcu.edu>
2021-03-05 00:15:39 -0500

    Winner page button

--- app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
+++ app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
@@ -1,13 +1,9 @@
 package com.example.minigolfscoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
-
 import android.os.Bundle;
 
 public class WinnerActivity extends AppCompatActivity {
@@ -15,13 +11,23 @@ public class WinnerActivity extends AppCompatActivity {
     int firstScore;
     int secondScore;
     int thirdScore;
+    Button playagain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner);
         Intent intent = getIntent();
         firstScore = intent.getIntExtra("firstScore", 0);
+
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_winner);
+        this.playagain = (Button) this.findViewById(R.id.winner);
+        this.playagain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
 
