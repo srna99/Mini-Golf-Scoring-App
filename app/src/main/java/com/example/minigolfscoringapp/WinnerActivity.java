170f66b115a0d618b44e663f34de5847c1dfe5af
Ankur Patel <patelas23@vcu.edu>
2021-03-25 14:32:57 -0400

    corrected behavior for quit and play again buttons

--- app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
+++ app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
@@ -33,32 +33,36 @@ public class WinnerActivity extends AppCompatActivity {
         btn_quit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                Intent startIntent = new Intent(getBaseContext(), StartActivity.class);
+//                startActivity(startIntent);
+                finish();
+            }
+        });
+
+        btn_play_again = (Button) findViewById(R.id.playAgainButton);
+        btn_play_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent startIntent = new Intent(getBaseContext(), StartActivity.class);
                 startActivity(startIntent);
             }
         });
-
-//        btn_quit = (Button) findViewById(R.id.quitButton);
-//        btn_quit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                finish();
-//            }
-//        });
-
-    }
+    }//END onCreate
 
     private void updateTopScoreLabels(int[] topScores) {
         TextView lbl_first_place, lbl_second_place, lbl_third_place;
 
         lbl_first_place = (TextView) findViewById(R.id.firstPlace);
         lbl_second_place = (TextView) findViewById(R.id.secondPlace);
+
 //        lbl_third_place = (TextView) findViewById(R.id.thirdPlace);
 
         lbl_first_place.setText("1st:\t" + playerNames[0] + " - " + topScores[0]);
         lbl_second_place.setText("2nd:\t" + playerNames[1] + " - " + topScores[1]);
+
 //        lbl_third_place.setText("Third Place: " + topScores[2]);
 
-    }
-}
+    }//END updateTopScoreLabels
+
+}//END WinnerActivity
 
