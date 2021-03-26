687f10cbd575505f2a451295fc61ee83bf97bd62
Serena Cheng <serena.ch1999@gmail.com>
2021-03-26 18:03:06 -0400

    Fix fonts and layouts, and update score displays

--- app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
+++ app/src/main/java/com/example/minigolfscoringapp/WinnerActivity.java
@@ -38,26 +38,32 @@ public class WinnerActivity extends AppCompatActivity {
             }
         });
 
-//        btn_quit = (Button) findViewById(R.id.quitButton);
-//        btn_quit.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                finish();
-//            }
-//        });
+        btn_quit = (Button) findViewById(R.id.quitButton);
+        btn_quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 
     private void updateTopScoreLabels(int[] topScores) {
-        TextView lbl_first_place, lbl_second_place, lbl_third_place;
+        TextView lbl_first_place, lbl_second_place, lbl_third_place, firstPlaceScoreLabel, secondPlaceScoreLabel;
 
-        lbl_first_place = (TextView) findViewById(R.id.firstPlace);
-        lbl_second_place = (TextView) findViewById(R.id.secondPlace);
+        lbl_first_place = (TextView) findViewById(R.id.firstPlaceName);
+        lbl_second_place = (TextView) findViewById(R.id.secondPlaceName);
 //        lbl_third_place = (TextView) findViewById(R.id.thirdPlace);
 
-        lbl_first_place.setText("1st:\t" + playerNames[0] + " - " + topScores[0]);
-        lbl_second_place.setText("2nd:\t" + playerNames[1] + " - " + topScores[1]);
+        firstPlaceScoreLabel = (TextView) findViewById(R.id.firstPlaceScore);
+        secondPlaceScoreLabel = (TextView) findViewById(R.id.secondPlaceScore);
+
+        lbl_first_place.setText(playerNames[0]);
+        lbl_second_place.setText(playerNames[1]);
 //        lbl_third_place.setText("Third Place: " + topScores[2]);
+
+        firstPlaceScoreLabel.setText(String.valueOf(topScores[0]));
+        secondPlaceScoreLabel.setText(String.valueOf(topScores[1]));
 
     }
 }
