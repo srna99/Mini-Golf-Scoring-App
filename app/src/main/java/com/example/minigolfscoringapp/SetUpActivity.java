0448a696de58e561085662f7283e5ca2e47ce2b2
Ankur Patel <patelas23@vcu.edu>
2021-03-28 22:09:24 -0400

    Button navigation to scoresheet

--- app/src/main/java/com/example/minigolfscoringapp/SetUpActivity.java
+++ app/src/main/java/com/example/minigolfscoringapp/SetUpActivity.java
@@ -1,14 +1,30 @@
 package com.example.minigolfscoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SetUpActivity extends AppCompatActivity {
+    Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up);
+
+//        setupContinueButton();
+        continueButton = findViewById(R.id.continueButton);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ScoreSheetActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void setupNumPlayersInput() {
@@ -20,6 +36,7 @@ public class SetUpActivity extends AppCompatActivity {
     }
 
     private void setupContinueButton() {
+
 
     }
 
