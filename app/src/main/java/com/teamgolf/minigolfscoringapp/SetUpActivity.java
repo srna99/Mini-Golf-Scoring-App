18dbe2e4bc3a07799679dd7c5629a5d7f1ca716e
alexolek <43213914+alexolek@users.noreply.github.com>
2021-04-27 17:02:17 -0400

    Update SetUpActivity.java

--- app/src/main/java/com/teamgolf/minigolfscoringapp/SetUpActivity.java
+++ app/src/main/java/com/teamgolf/minigolfscoringapp/SetUpActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,19 +31,28 @@ public class SetUpActivity extends AppCompatActivity {
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (TextUtils.isEmpty(playersIn.getText().toString())){
+                    playersIn.setError("Please enter a number of players between 1 and 4");
+                }
+                if (TextUtils.isEmpty(holesIn.getText().toString())) {
+                    holesIn.setError("Please enter a number of holes between 1 and 18");
+                }
                 Intent scoreSheetIntent = new Intent(getBaseContext(), ScoreSheetActivity.class);
 
                 numPlayers = Integer.parseInt(playersIn.getText().toString());
                 if( numPlayers <1 || numPlayers >4) {
                     playersIn.setError("Please enter a number of players between 1 and 4");
                 }
+
                 numHoles = Integer.parseInt(holesIn.getText().toString());
                 if( numHoles <1 || numHoles >18) {
                     holesIn.setError("Please enter a number of holes between 1 and 18");
                 }
-                scoreSheetIntent.putExtra("numberOfPlayers", numPlayers);
-                scoreSheetIntent.putExtra("numberOfHoles", numHoles);
-                if (numHoles >0 && numHoles <19 && numPlayers >0 && numPlayers < 5) {
+
+                
+                if (playersIn.getError() == null && holesIn.getError() == null) {
+                    scoreSheetIntent.putExtra("numberOfPlayers", numPlayers);
+                    scoreSheetIntent.putExtra("numberOfHoles", numHoles);
                     startActivity(scoreSheetIntent);
                 }
             }
