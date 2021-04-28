a6b75b63b1b3b968e6a06ae32d350925164d4f68
Ankur Patel <patelas23@vcu.edu>
2021-04-27 23:44:31 -0400

    Using flow layout to align dynamic text input fields

--- app/src/main/java/com/teamgolf/minigolfscoringapp/NameInputActivity.java
+++ app/src/main/java/com/teamgolf/minigolfscoringapp/NameInputActivity.java
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.icu.number.UnlocalizedNumberFormatter;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.w3c.dom.Text;
 
 public class NameInputActivity extends AppCompatActivity {
+
+
+    EditText nameInput;
+    TextInputLayout namesInLayout;
 
     int numPlayers = 2;
 
@@ -15,12 +24,18 @@ public class NameInputActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_input);
 
+        nameInput = (EditText) findViewById(R.id.playerName1);
+        namesInLayout = (TextInputLayout) findViewById(R.id.nameInputsLayout);
+
 //        Intent receivedIntent = getIntent();
 //        numPlayers = receivedIntent.getIntExtra("numberOfPlayers", 2);
 
 
         //Dynamically create name input fields, with a default amount of 1
         for(int i=1; i<=numPlayers; i++) {
+            //Create new name input with incrementing id
+
+            //Add name input field to text input layout
 
         }
 
