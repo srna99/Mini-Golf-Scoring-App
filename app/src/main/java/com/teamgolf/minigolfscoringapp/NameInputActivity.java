c751a1e7b70dfca53ad80db2a29c0660946872d4
Ankur Patel <patelas23@vcu.edu>
2021-04-29 17:57:06 -0400

    dynamic creation of input fields

--- app/src/main/java/com/teamgolf/minigolfscoringapp/NameInputActivity.java
+++ app/src/main/java/com/teamgolf/minigolfscoringapp/NameInputActivity.java
@@ -2,10 +2,14 @@ package com.teamgolf.minigolfscoringapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.icu.number.UnlocalizedNumberFormatter;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -16,29 +20,41 @@ public class NameInputActivity extends AppCompatActivity {
 
     EditText nameInput;
     TextInputLayout namesInLayout;
+    LinearLayout parentLinearLayout;
 
     int numPlayers = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_name_input);
+        setContentView(R.layout.activity_player_name_input);
 
-        nameInput = (EditText) findViewById(R.id.playerName1);
-        namesInLayout = (TextInputLayout) findViewById(R.id.nameInputsLayout);
+
+
+//        nameInput = (EditText) findViewById(R.id.playerName1);
+//        namesInLayout = (TextInputLayout) findViewById(R.id.nameInputsLayout);
 
 //        Intent receivedIntent = getIntent();
 //        numPlayers = receivedIntent.getIntExtra("numberOfPlayers", 2);
 
 
-        //Dynamically create name input fields, with a default amount of 1
-        for(int i=1; i<=numPlayers; i++) {
-            //Create new name input with incrementing id
+    }
 
-            //Add name input field to text input layout
+    //Dynamically create name input fields, with a default amount of 1
+    protected void addInputFields(int n) {
+        parentLinearLayout = (LinearLayout) findViewById(R.id.name_parent_linear_layout);
+
+        for(int i=2; i<=numPlayers; i++) {
+            //Create new name input with incrementing id
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout tempLine = (LinearLayout) inflater.inflate(R.layout.name_input_field, null);
+            EditText nextField = (EditText) tempLine.getChildAt(0);
+            nextField.setHint("Player " + i);
+//            nextField.setId("player_" + i + "_field");
+            //Add subsequent fields before submit button
+            parentLinearLayout.addView(nextField, parentLinearLayout.getChildCount() - 1);
+
 
         }
-
-
     }
 }
